47a8e2b0be8ab04dbca12e4fe94c5a0c3594197e
mihoyo-typ <32317812+mihoyo-typ@users.noreply.github.com>
2025-08-24 14:53:30 +0800

    更新 testflight.js

--- testflight.js
+++ testflight.js
@@ -116,7 +116,7 @@ function getResult() {
       else {
         var nostr ='[' + name + ']' + '\n'+ logdata[name].context;
          if (isNOtify == "false") {
-        //$.msg("TestFilght监控","",nostr);
+        $.msg("TestFilght监控","",nostr);
       }
       else{
        $.log("TestFilght监控","",nostr);
