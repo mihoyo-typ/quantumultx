f2730792504d44dc3994b343d14905076540c5dc
mihoyo-typ <32317812+mihoyo-typ@users.noreply.github.com>
2025-08-24 15:19:23 +0800

    更新 testflight.js

--- testflight.js
+++ testflight.js
@@ -109,9 +109,9 @@ function getResult() {
       if (logdata[name].has) {
         var hastr ='[' + name + ']' +'\n'+ logdata[name].context;
          
-        // $.msg("TestFilght监控","",hastr){
-          //  "open-url": hastr
-       // });
+         $notify("TestFilght监控","",hastr){
+            "open-url": hastr
+        });
 //const roll = "https://testflight.apple.com/join/gdE4pRzI";
 
         $notify("TestFilght", upstr + '👉:' +'\n'+ lol.url + '\n\n', "点击跳转浏览器下载本地", {
